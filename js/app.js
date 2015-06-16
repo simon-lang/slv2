3fb8dc2c3d2e594fcf17531a11431e8bea64b635
Simon Lang <simon@codeworx.com.au>
2015-06-16 14:55:28 +1000

    Updates

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var makeTimesheet;
+
 _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 
 $(function() {
-  var email, externalLinks, hash;
+  var companies, email, externalLinks, hash, html;
   $.ajax({
     url: "https://api.github.com/users/captainclam/gists"
   }).done(function(gists) {
@@ -33,6 +35,7 @@ $(function() {
     var href;
     e.preventDefault();
     window.location.hash = $(this).attr('href');
+    $('html,body').scrollTop(0);
     href = $(this).attr('href');
     $('a').removeClass('active');
     $(this).addClass('active');
@@ -63,8 +66,18 @@ $(function() {
     $(hash).show();
   }
   email = ['me', '@', 'simonlang', '.org'].join('');
-  return $('.mailto').attr('href', 'mailto:' + email);
+  $('.mailto').attr('href', 'mailto:' + email);
+  html = $('#employment').html();
+  companies = ['Codeworx', 'Digicon', 'Open Platform', 'Self Employed', 'Kintek.com.au', 'University of Queensland', 'Daily Mercury'];
+  _.each(companies, function(c) {
+    return html = html != null ? html.replace(c, "<strong>" + c + "</strong>") : void 0;
+  });
+  return setTimeout(makeTimesheet, 2000);
 });
+
+makeTimesheet = function() {
+  return new Timesheet('timesheet', 2002, 2013, [['2002', '09/2002', 'A freaking awesome time', 'lorem'], ['06/2002', '09/2003', 'Some great memories', 'ipsum'], ['2003', 'Had very bad luck'], ['10/2003', '2006', 'At least had fun', 'dolor'], ['02/2005', '05/2006', 'Enjoyed those times as well', 'ipsum'], ['07/2005', '09/2005', 'Bad luck again', 'default'], ['10/2005', '2008', 'For a long time nothing happened', 'dolor'], ['01/2008', '05/2009', 'LOST Season #4', 'lorem'], ['01/2009', '05/2009', 'LOST Season #4', 'lorem'], ['02/2010', '05/2010', 'LOST Season #5', 'lorem'], ['09/2008', '06/2010', 'FRINGE #1 & #2', 'ipsum']]);
+};
 
 
 },{}]},{},[1]);

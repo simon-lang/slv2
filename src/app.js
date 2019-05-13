0be653ca1fee143d95e6c3b391750b54b92cfd2c
Simon Lang <me@simonlang.org>
2019-05-13 14:51:03 +1000

    Misc Updates

--- src/app.js
+++ src/app.js
@@ -1,19 +1,35 @@
 window.addEventListener('load', (event) => {
 
+    const toggleReadMore = (container) => {
+        const el = container.querySelector('.read-more-content')
+        el.classList.toggle('hidden')
+        const hidden = el.classList.contains('hidden')
+        const link = container.querySelector('.read-more')
+        link.innerText = hidden ? 'more info...' : 'collapse...'
+        el.querySelectorAll('img').forEach(img => {
+            var src = img.getAttribute('lazy-src')
+            if (src) {
+                img.setAttribute('src', src)
+            }
+        })
+    }
+
+    document.querySelectorAll('.read-more-items > li').forEach(el => {
+        el.addEventListener('click', function (e) {
+            toggleReadMore(e.target)
+        })
+    })
+
     document.querySelectorAll('.read-more').forEach(d => {
         d.addEventListener('click', function (e) {
             e.preventDefault()
-            const el = e.target
-            const child = el.parentElement.querySelector('.read-more-content')
-            child.classList.toggle('hidden')
-            const hidden = child.classList.contains('hidden')
-            el.innerText = hidden ? 'more info...' : 'collapse...'
-            child.querySelectorAll('img').forEach(img => {
-                var src = img.getAttribute('lazy-src')
-                if (src) {
-                    img.setAttribute('src', src)
-                }
-            })
+            toggleReadMore(e.target.parentElement)
+        })
+    })
+
+    document.querySelectorAll('.read-more-content').forEach(el => {
+        el.addEventListener('click', e => {
+            e.stopPropagation()
         })
     })
 })
